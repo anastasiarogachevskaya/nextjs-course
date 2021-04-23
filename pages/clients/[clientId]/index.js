b44d7e0f8fb522dec5d3ba17a12980f4c258da53
Anastasia <anastasia.rogachevskaya@aller.com>
2021-04-23 15:00:31 +0300

    programmatic nav

--- pages/clients/[clientId]/index.js
+++ pages/clients/[clientId]/index.js
@@ -1,8 +1,22 @@
+import { useRouter } from 'next/router';
+//programmatic navigation
 
 function ClientsProjectsPage() {
+  const router = useRouter();
+  
+  function loadProjectHandler() {
+    // router.push('/clients/nastya/2'); // ability to go back
+    // router.replace('/clients/nastya/2'); // no ability to go back
+    router.push({
+      pathname: '/clients/[id]/[clientproject]',
+      query: { id: 'nastya', clientproject: '3'},
+    })
+  }
+
   return(
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
