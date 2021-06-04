e8a15afc565fe262ba1ae1f946042d552792e67f
Anastasia <anastasia.rogachevskaya@aller.com>
2021-06-04 13:47:58 +0300

    Head element for <head>

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 // Path - /
+import Head from 'next/head';
 import { getFeaturedEvents } from '../helpers/apiUtil';
 import EventList from '../components/events/EventList';
 
 function HomePage({events}) {
   return(
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name="description" content="Hello I am description" />
+      </Head>
       <EventList events={events} />
     </div>
   )
