e6895153e10e8b48d9e8da70bdcd51f5d1ccf08a
Anastasia <anastasia.rogachevskaya@aller.com>
2021-11-19 12:00:39 +0200

    sending newsletter subscribers to the DB

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,12 +1,19 @@
+import { MongoClient } from 'mongodb';
+
 import validateEmail from '../../utils/validateEmail';
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email } = req.body;
     if (!validateEmail(email)) {
       res.status(422).json({ message: 'Invalid email' });
       return;
     }
+
+    const client =await MongoClient.connect('mongodb+srv://mtgAdmin:Zqm55Dqub9Uxa5jc@cluster0.fkmmx.mongodb.net/nextjs_project?retryWrites=true&w=majority');
+    const db = client.db('nextjs_project');
+    await db.collection('newsletter').insertOne({ email });
+    client.close();
 
     console.log(email);
     res.status(201).json({ message: 'Signed up!' });
