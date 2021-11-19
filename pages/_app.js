76e7ee5c7db9885fdec85d9676ef15e689c792b4
Anastasia <anastasia.rogachevskaya@aller.com>
2021-11-19 09:49:04 +0200

    project initialization

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
-import Layout from '../components/layout/Layout';
-import '../styles/globals.css'
+import Head from 'next/head';
 
-const MyApp = ({ Component, pageProps }) => {
+import Layout from '../components/layout/layout';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Next Events</title>
+        <meta name='description' content='NextJS Events' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
